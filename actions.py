2ff2d2311f8b1f7b2d187013bb7e7f68bfeb751f
MrPaul3 <87758775+AritraPal3@users.noreply.github.com>
2023-09-04 17:00:46 +0530

    custom action prb resolved

--- actions.py
+++ actions.py
@@ -1,7 +1,7 @@
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from typing import Any, Text, Dict, List
-
+import random
 
 # class ActionName(Action):
 #     def name(self) -> Text:
@@ -53,5 +53,25 @@ class Action_Fallback(Action):
     def name(self) -> Text:
         return "action_fallback"
     def run(self,dispatcher:CollectingDispatcher,tracker:Tracker,domain:Dict[Text,Any]) ->List[Dict[Text,Any]]:
-        dispatcher.utter_message(response="utter_fallback")
+        dispatcher.utter_message(response="utter_default")
+        return []
+    
+class Action_Explain(Action):
+    def name(self)->Text:
+        return "action_ans_explain"
+    def run(self,dispatcher:CollectingDispatcher,tracker:Tracker,domain:Dict[Text,Any])->List[Dict[Text,Any]]:
+        anscontainer=[
+            "A provident fund is a financial savings scheme designed to help individuals save money for their retirement.",
+            "A provident fund is a long-term savings account that aims to provide financial security to individuals after they retire from work.",
+            "A provident fund, often abbreviated as 'PF' is a retirement-oriented investment fund where employees and employers contribute a portion of the employee's salary regularly.",
+            "A provident fund is a tax-advantaged retirement savings plan that enables employees to set aside a portion of their income for their future retirement needs.",
+            "A provident fund is a type of investment fund established by employers to assist employees in building a financial cushion for retirement.",
+            "A provident fund is a retirement savings scheme that offers employees a disciplined way to accumulate funds for their post-retirement life.",
+            "Provident funds are investment vehicles that help individuals save money over their working years, with the aim of providing financial support during retirement.",
+            "A provident fund is a financial instrument that encourages long-term savings and investments to ensure financial stability during one's retirement years.",
+            "Provident funds are structured savings plans that facilitate systematic retirement planning by both employees and employers.",
+            "A provident fund is a retirement benefit scheme that offers financial protection to employees in their post-retirement years.",
+        ]
+        answer=random.choice(anscontainer)
+        dispatcher.utter_message(text=answer)
         return []
